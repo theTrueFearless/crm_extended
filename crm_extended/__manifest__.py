856fe987f140223259d231225373fff38f41bf3e
Derly Gonzalez <derlin@gmail.com>
2025-06-25 17:09:41 -0400

    [ADD] crm_extended module

---
+++ crm_extended/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Extended CRM',
+    'version': '16.0',
+    'summary': 'Summery',
+    'description': 'Description',
+    'category': 'CRM',
+    'author': 'Derly González',
+    'website': 'Website',
+    'license': 'LGPL-3',
+    'depends': [
+        'crm',
+        'website'],
+    'data': [
+        # security
+        'security/ir.model.access.csv'
+        # views
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    "application": False,
+    'auto_install': False
+}
