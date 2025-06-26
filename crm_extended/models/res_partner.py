d73fca7732aa9a7685923beef23f5b7b250d6c78
Derly Gonzalez <derlin@gmail.com>
2025-06-26 19:10:36 -0400

    [IMP] crm_extended: added "Profile complete" feature

--- crm_extended/models/res_partner.py
+++ crm_extended/models/res_partner.py
@@ -6,3 +6,13 @@ class ResPartner(models.Model):
 
     social_network_profile_ids = fields.One2many('crm_extended.social.network.profile', 'partner_id',
                                                  string='Social Profiles')
+    completed_profile = fields.Boolean(compute='_compute_profile_social_complete')
+
+    @api.depends('social_network_profile_ids.network_id')
+    def _compute_profile_social_complete(self):
+        all_networks = self.env['crm_extended.social.network'].search([])
+        all_network_ids = set(all_networks.ids)
+
+        for record in self:
+            customer_network_ids = set(record.social_network_profile_ids.mapped('network_id').ids)
+            record.completed_profile = all_network_ids.issubset(customer_network_ids)
