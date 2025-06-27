fddbd6a078e9df9c06249da4a2c195f5e6e5b7dc
Derly Gonzalez <derlin@gmail.com>
2025-06-27 08:02:19 -0400

    [IMP] crm_extended: added validations to social media management

--- crm_extended/models/crm_extended_social_network.py
+++ crm_extended/models/crm_extended_social_network.py
@@ -1,4 +1,5 @@
-from odoo import fields, models, api
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class CrmExtendedSocialNetwork(models.Model):
@@ -7,4 +8,15 @@ class CrmExtendedSocialNetwork(models.Model):
 
     name = fields.Char(required=True)
     icon = fields.Binary(required=True)
+
+    @api.constrains('name')
+    def _check_unique_name(self):
+        for record in self:
+            if self.search_count([
+                ('id', '!=', record.id),
+                ('name', 'ilike', record.name.strip())
+            ]) > 0:
+                raise ValidationError(_(
+                    "A social network named '%s' already exists (names are case-insensitive)."
+                ) % record.name.strip())
 
--- crm_extended/models/crm_extended_social_network_profile.py
+++ crm_extended/models/crm_extended_social_network_profile.py
@@ -1,4 +1,5 @@
-from odoo import fields, models, api
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class CrmExtendedSocialNetworkProfile(models.Model):
@@ -9,3 +10,15 @@ class CrmExtendedSocialNetworkProfile(models.Model):
     partner_id = fields.Many2one('res.partner', string='Customer', required=True, ondelete='cascade')
     network_id = fields.Many2one('crm_extended.social.network', string='Social Network', required=True)
     icon = fields.Binary(string="Icon", related='network_id.icon', store=False)
+
+    _sql_constraints = [
+        ('unique_profile', 'UNIQUE(partner_id, network_id)',
+         _('Each customer can only have one profile per social network!')),
+    ]
+
+    @api.constrains('url')
+    def _check_valid_url(self):
+        for record in self:
+            if self.url:
+                if not record.url.startswith(('http://', 'https://')) or not "." in record.url:
+                    raise ValidationError(_("URL must start with http:// or https:// and have '.' inside"))
